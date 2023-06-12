d74b7a57928cdb9b7717f83e9727707152805b9e
Neeltech92 <115792985+Neeltech92@users.noreply.github.com>
2023-06-12 08:28:11 +0000

    dsfs

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
-let password1El = document.getElementById("password1-el")
-let password2El = document.getElementById("password2-el")
-let numChar = document.getElementById("num-char")
+const password1El = document.getElementById("password1-el")
+const password2El = document.getElementById("password2-el")
+const numChar = document.getElementById("num-char")
+const Btn = document.getElementById("btn")
+
 
 function getPassword() {
     password1El.textContent = ""
     password2El.textContent = ""
-    for (i=1; i<=numChar.value; i++) {
+    for (let i=1; i<=numChar.value; i++) {
     let x = Math.floor(Math.random()*characters.length)
     let y = Math.floor(Math.random()*characters.length)
     password1El.textContent += characters[x]
@@ -16,5 +18,4 @@ function getPassword() {
     }
 }
 
-
-
+Btn.addEventListener("click", getPassword)
